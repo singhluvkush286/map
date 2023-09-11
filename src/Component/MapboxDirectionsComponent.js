be8634ee102462a61ad652707d8993f2fce5dda4
Luvkush Singh <singhlavkush286@gmail.com>
2023-09-11 23:30:53 +0530

    Update

--- src/Component/MapboxDirectionsComponent.js
+++ src/Component/MapboxDirectionsComponent.js
@@ -8,17 +8,42 @@ const cspWorkerUrl = require('mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibHV2MjEwMTIiLCJhIjoiY2xrd2h4MTRnMDNocTNjbnphMHIyOGYxbCJ9.1cAcp80juY-TkemwZ9UPOw';
 
-mapboxgl.workerClass = cspWorkerUrl;
-
 const MapboxDirectionsComponent = ({ origin, destination }) => {
   const [instructions, setInstructions] = useState([]);
   const [userCoordinates, setuserCoordinates] = useState([]);
-  const [currentInstructionIndex, setCurrentInstructionIndex] = useState(0); 
-  const [isNavigationStarted, setNavigationStarted] = useState(false); 
+  const [currentInstructionIndex, setCurrentInstructionIndex] = useState(0);
+  const [isNavigationStarted, setNavigationStarted] = useState(false);
 
   const mapContainerRef = useRef(null);
   const geolocateControlRef = useRef(null);
   const mapRef = useRef(null);
+
+  // Function to remove the navigation line
+  const removeNavigationLine = () => {
+    const map = mapRef.current;
+    map.removeLayer('route');
+    map.removeSource('route');
+  };
+
+  const createMarkers = () => {
+    const map = mapRef.current;
+
+    // Add the green marker for the starting point
+    new mapboxgl.Marker({
+      color: 'limegreen',
+    })
+      .setLngLat(origin)
+      .addTo(map)
+      .setPopup(new mapboxgl.Popup().setText('Starting Point'));
+
+    // Add the red marker for the destination point
+    new mapboxgl.Marker({
+      color: 'red',
+    })
+      .setLngLat(destination)
+      .addTo(map)
+      .setPopup(new mapboxgl.Popup().setText('Destination Point'));
+  };
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -48,52 +73,46 @@ const MapboxDirectionsComponent = ({ origin, destination }) => {
     };
   }, []);
 
-  const createArrowElement = (color) => {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('width', '24');
-    svg.setAttribute('height', '24');
-    svg.innerHTML = `
-      <path fill="${color}" d="M0 0h24v24H0z" fill-opacity="0"/>
-      <path d="M12 2c-.14 0-.27.03-.39.08l-.52.23-.65.34-.82.53-.9.68L7 4v16h10V4l-1.72.57-.9-.68-.82-.53-.65-.34-.52-.23C12.27 2.03 12.14 2 12 2zm-2 3.5l1.79 1.03L12 7l1.21-.47L14 5.5l-1-.63-1 .63zm4 11l-3-3v2H9v-2l-3 3 3 3v-2h6v2l3-3zm-3-11.97L12 8.6l-.79-.46-.71-.41V5.53l-.29-.09L11 5l.71-.41L12 4.4l.29.09L13 5v1.66l-.71.41zM13 12v1H9v-1l-1-1v3h6v-3l-1 1zm-1-4v-.66l-.71-.41L11 6.4l-.29.09L10 7V6.52l-.71.41-.71.41-.79.46 1 1 .79.46.71.41V8l.21-.08L12 7.94zm3.44 7.28l.71-.41.71-.41.79-.46-1-1-.79-.46-.71-.41V16l-.21.08L14 16.06v.66l.71.41.71.41.79.46-1-1-.79-.46-.71-.41V17l-.21.08z"/>
-    `;
-    return svg;
-  };
-  
-
   const drawNavigationLine = (coordinates) => {
     const map = mapRef.current;
-    
-      map.addSource('route', {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          properties: {},
-          geometry: {
-            type: 'LineString',
-            coordinates: coordinates,
-          },
-        },
-      });
-  
-      map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'route',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round',
-        },
-        paint: {
-          'line-color': '#3887be',
-          'line-width': 5,
-          'line-opacity': 0.75,
-        },
-      });
-    };
 
+    map.addSource('route', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: coordinates,
+        },
+      },
+    });
+
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: 'route',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round',
+      },
+      paint: {
+        'line-color': '#3887be',
+        'line-width': 5,
+        'line-opacity': 0.75,
+      },
+    });
+  };
 
   const handleStartNavigation = () => {
     setNavigationStarted(true);
+
+    // Start tracking the user's location
+    const geolocateControl = geolocateControlRef.current;
+    geolocateControl.trigger();
+
+    // Add markers for starting and destination points
+    // createMarkers();
   };
 
   useEffect(() => {
@@ -107,32 +126,22 @@ const MapboxDirectionsComponent = ({ origin, destination }) => {
             `https://api.mapbox.com/directions/v5/mapbox/walking/${origin[0]},${origin[1]};${destination[0]},${destination[1]}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=${mapboxgl.accessToken}`
           );
 
-          
           const data = await response.json();
           console.log('Directions API Response:', data);
 
-          if (data.routes && data.routes.length > 0 && data.routes[0].legs[0].steps && data.routes[0].legs[0].steps.length > 0) {
+
+
+          if (
+            data.routes &&
+            data.routes.length > 0 &&
+            data.routes[0].legs[0].steps &&
+            data.routes[0].legs[0].steps.length > 0
+          ) {
             setInstructions(data.routes[0].legs[0].steps);
             console.log(data.routes[0].legs[0].steps);
             // Extract the coordinates from the instructions and draw the navigation line
             const coordinates = data.routes[0].geometry.coordinates;
             drawNavigationLine(coordinates);
-
-            // Add the arrow marker for the starting point
-            new mapboxgl.Marker({
-              element: createArrowElement('limegreen'),
-            })
-              .setLngLat(origin)
-              .addTo(map)
-              .setPopup(new mapboxgl.Popup().setText('Starting Point'));
-
-            // Add the arrow marker for the destination point
-            new mapboxgl.Marker({
-              element: createArrowElement(),
-            })
-              .setLngLat(destination)
-              .addTo(map)
-              .setPopup(new mapboxgl.Popup().setText('Destination Point'));
 
             // Initialize instruction display
             setCurrentInstructionIndex(0);
@@ -145,63 +154,42 @@ const MapboxDirectionsComponent = ({ origin, destination }) => {
       };
 
       fetchDirections();
+      createMarkers();
     }
 
     if (isNavigationStarted) {
       const geolocateControl = geolocateControlRef.current;
-      
-      if(navigator.geolocation)
-      {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setuserCoordinates([position.coords.latitude,position.coords.longitude])
-          },
-          () => {
-            console.log("Unable to retrieve your location")
-          });
-      }
-      else{
-        console.log("Geolocation not Supported");
-      }
-      console.log(userCoordinates)
-      if (currentInstructionIndex < instructions.length - 1) 
-      {
-            const currentStep = instructions[currentInstructionIndex];
-            const stepCoordinates = currentStep.maneuver.location;
-            console.log("usercoord "+ userCoordinates);
-            console.log("usercoord "+ typeof(userCoordinates));
-            console.log("stepcoords "+ stepCoordinates);
-            console.log("usercoord "+ typeof(stepCoordinates));
-            // const userCoords = Object.values(userCoordinates);
-            // const stepCoords = Object.values(stepCoordinates)
-            var from = point([userCoordinates['0'],userCoordinates['1']]);
-            var to = point([stepCoordinates['0'],userCoordinates['1']]);
-            // console.log("from:"+typeof(from)+"  To:"+typeof(to))
-            //const distanceToStep = mapRef.current.distance(userCoordinates, stepCoordinates);
-            const distanceToStep = distance(from,to)
 
-            console.log("Distance b/w "+ distanceToStep);
-            if (distanceToStep < 20) 
-            { // Adjust the threshold as needed
-              setCurrentInstructionIndex(currentInstructionIndex + 1);
-            }
+      const geolocateHandler = (position) => {
+        setuserCoordinates([position.coords.latitude, position.coords.longitude]);
+
+        if (currentInstructionIndex < instructions.length - 1) {
+          const currentStep = instructions[currentInstructionIndex];
+          const stepCoordinates = currentStep.maneuver.location;
+
+          const from = point([userCoordinates[1], userCoordinates[0]]);
+          const to = point([stepCoordinates[1], stepCoordinates[0]]);
+          const distanceToStep = distance(from, to);
+
+          console.log('Distance between user and step: ' + distanceToStep);
+
+          if (distanceToStep < 20) {
+            setCurrentInstructionIndex(currentInstructionIndex + 1);
+          }
+        } else {
+          // All instructions completed, remove the navigation line
+          removeNavigationLine();
         }
-      const geolocateHandler = () => {
-        navigator.geolocation.watchPosition(userCoordinates);
-        //console.log(updateUserLocation);
       };
-  
+
       geolocateControl.on('geolocate', geolocateHandler);
-  
+
       return () => {
         geolocateControl.off('geolocate', geolocateHandler);
       };
-
-    
     }
-  }, [origin, destination, isNavigationStarted,userCoordinates, instructions,currentInstructionIndex]);
+  }, [origin, destination, isNavigationStarted, userCoordinates, instructions, currentInstructionIndex]);
 
- 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div ref={mapContainerRef} style={{ width: '100%', height: '300px', marginBottom: '8px' }} />
@@ -227,16 +215,13 @@ const MapboxDirectionsComponent = ({ origin, destination }) => {
           </button>
         )}
       </div>
-      
-      {/* Display current instruction */}
       {isNavigationStarted && instructions.length > 0 && (
         <div style={{ width: '100%', maxWidth: '400px', padding: '10px', border: '1px solid #ccc', marginTop: '8px' }}>
           <h3>Current Instruction</h3>
           <p>{instructions[currentInstructionIndex].maneuver.instruction}</p>
-         
         </div>
       )}
-    </div>  
+    </div>
   );
 };
 
